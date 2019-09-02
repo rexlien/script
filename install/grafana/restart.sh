48e5dd65d8a7b889da46e137d2fcc30a60d6c161
rex.lien <line@TW-LW10219-M-2.local>
2019-09-02 17:50:26 +0800

    Update

--- install/grafana/restart.sh
+++ install/grafana/restart.sh
@@ -3,4 +3,4 @@ docker volume create grafana-storage
 # start grafana
 docker stop grafana | true
 docker rm grafana | true
-docker run -d --restart=always -p 3000:3000 --name=grafana -v grafana-storage:/var/lib/grafana grafana/grafana
+docker run -d --restart=always -p 3000:3000 --name=grafana -v grafana-storage:/var/lib/grafana -e "GF_INSTALL_PLUGINS=grafana-piechart-panel" grafana/grafana 
