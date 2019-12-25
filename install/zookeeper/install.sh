4fb96bd3c8006361f161291f0606f90cc572d2fe
rex-lien <line@TW-LW10219-M-2.local>
2019-12-25 16:45:20 +0800

    Update

--- install/zookeeper/install.sh
+++ install/zookeeper/install.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+set -euo pipefail
+
+BASEDIR=$(dirname "$0")
+echo "$BASEDIR"
 
 ZK_HOST1=$1
 ZK_HOST2=$2
 ZK_HOST3=$3
 MY_ID=$4
 
-mkdir zookeeper
-tar -xvzf apache-zookeeper-3.5.6-bin.tar.gz -C zookeeper
+mkdir -p zookeeper
+tar -xvzf $BASEDIR/apache-zookeeper-3.5.6-bin.tar.gz -C zookeeper
 chmod 777 ./zookeeper/apache-zookeeper-3.5.6-bin/conf
-rm ./zookeeper/apache-zookeeper-3.5.6-bin/conf/zoo.cfg | true
+rm ./zookeeper/apache-zookeeper-3.5.6-bin/conf/zoo.cfg || true
 cat >> ./zookeeper/apache-zookeeper-3.5.6-bin/conf/zoo.cfg <<EOF
 tickTime=2000
 dataDir=./zookeeper-data/data
@@ -22,7 +26,8 @@ server.1=${ZK_HOST1}:2888:3888
 server.2=${ZK_HOST2}:2888:3888
 server.3=${ZK_HOST3}:2888:3888
 EOF
+mkdir -p ./zookeeper-data/data
 chmod 777 ./zookeeper-data/data
-sudo echo "${MY_ID}" > ./zookeeper-data/data/myid
-./zookeeper/apache-zookeeper-3.5.6-bin/bin/zkServer.sh stop | true
+sudo echo "${MY_ID}" > ./zookeeper-data/data/myid 
+./zookeeper/apache-zookeeper-3.5.6-bin/bin/zkServer.sh stop || true
 ./zookeeper/apache-zookeeper-3.5.6-bin/bin/zkServer.sh start
