98d732635a70746e17f454ae7f05906bec931272
rex.lien <rex.lien@linecorp.com>
2023-08-24 18:49:14 +0800

    optional mirror registry setting

--- init_centos.sh
+++ init_centos.sh
@@ -35,9 +35,15 @@ else
   "storage-opts": [
     "overlay2.override_kernel_check=true"
   ]
-}
 EOF
-    
+    if [[ -z "${XLN_DOCKER_MIRROR_REIGSTRY}" ]]; then
+      cat > /etc/docker/daemon.json <<EOF
+        "registry-mirrors": ["$XLN_DOCKER_MIRROR_REIGSTRY"]
+EOF
+    fi
+    cat > /etc/docker/daemon.json <<EOF
+  }
+EOF
     mkdir -p /etc/systemd/system/docker.service.d
 
     # Restart docker.
